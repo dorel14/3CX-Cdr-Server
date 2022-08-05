cac8121ce50abfb6a28cdf06bc11b7892d418d2b
David Orel <david.orel@sfr.fr>
2022-08-05 18:52:24 +0200

    modifif param docker

--- db_init/init-user-db.sh
+++ db_init/init-user-db.sh
@@ -2,7 +2,10 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username postgres --dbname 3cxcdr <<-EOSQL
+    GRANT CONNECT ON DATABASE 3cxcdr TO grafanareader;
     CREATE USER grafanareader WITH PASSWORD 'grafanareader';
-    GRANT SELECT ON DATABASE 3cxcdr TO grafanareader;
+    GRANT SELECT ON DATABASE 3cxcdr.* TO grafanareader;
+    GRANT SELECT ON ALL TABLES IN SCHEMA public TO grafanareader;
+    ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO grafanareader;
 EOSQL
 
