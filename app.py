ff253cbe2daf0122f39866dd4ee9fe3c212684a3
David Orel <david.orel@sfr.fr>
2023-05-22 20:01:24 +0200

    Préparation integration sqlite + streamlit front

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+from dynaconf import settings
+
+st.title('test d\' appli streamlit')
+st.write(settings)
---
+++ helpers/config.py
@@ -0,0 +1,25 @@
+# -*- coding: UTF-8 -*-
+import configparser
+import os
+
+
+temp_path = 'config/'  # os.path.dirname(os.path.abspath(__file__))
+configFile = os.path.join(temp_path, "config.ini")
+Config = configparser.RawConfigParser()
+Config.read(configFile)
+
+
+def initConfigModel():
+    Config.read(configFile)
+    ModelConfig = configparser.ConfigParser()
+    modelFile = os.path.join(temp_path, "model.ini")
+    ModelConfig.read(modelFile)
+    for s in ModelConfig.sections():
+        if s not in Config.sections():
+            Config.add_section(s)
+            Config.write(open(configFile, 'w'))
+        for o in ModelConfig.options(s):
+            if o not in Config.options(s):
+                value = ModelConfig.get(s, o)
+                Config.set(s, o, value)
+                Config.write(open(configFile, 'w'))
