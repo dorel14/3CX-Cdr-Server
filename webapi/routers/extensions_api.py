14063f9574e48327880ec07557f9cdbac652c28c
David Orel <david.orel@sfr.fr>
2023-05-30 17:10:34 +0200

    Correction docker compose + dockerfiles

---
+++ webapi/routers/extensions_api.py
@@ -0,0 +1,4 @@
+from fastapi import APIRouter
+from typing import Union
+import json
+
--- webapi/server.py
+++ webapi/server.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI
 from typing import Union
-from .routers import config_api
+from .routers import extensions_api
 
 app = FastAPI()
-app.include_router(config_api.router) #permet d'ajouter les routes d'un fichier externe
+app.include_router(extensions_api.router) #permet d'ajouter les routes d'un fichier externe
 
 @app.get('/')
 async def root():
