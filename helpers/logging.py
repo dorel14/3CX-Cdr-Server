8253512d602d2e18e8b72f82aedd7343d3584921
David Orel <david.orel@sfr.fr>
2024-04-24 19:07:27 +0200

    Merge branche streamlit app

---
+++ helpers/logging.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+import os
+import logging
+import pathlib
+
+from logging.handlers import RotatingFileHandler
+
+currentdir = os.path.dirname(os.path.realpath(__file__))
+parentdir = os.path.dirname(currentdir)
+logdir = os.path.join(parentdir, './logs')
+logfiles = os.path.join(logdir, '3cxtcpserver.log')
+
+pathlib.Path(logdir).mkdir(parents=True, exist_ok=True)
+print(parentdir)
+
+# création de l'objet logger qui va nous servir à écrire dans les logs
+logger = logging.getLogger()
+# on met le niveau du logger à DEBUG, comme ça il écrit tout
+logger.setLevel(logging.DEBUG)
+
+# création d'un formateur qui va ajouter le temps, le niveau
+# de chaque message quand on écrira un message dans le log
+formatter = logging.Formatter('%(asctime)s :: %(levelname)s :: %(message)s')
+# création d'un handler qui va rediriger une écriture du log vers
+# un fichier en mode 'append', avec 1 backup et une taille max de 1Mo
+file_handler = RotatingFileHandler(logfiles,
+                                   'a',
+                                   1000000,
+                                   1)
+# on lui met le niveau sur DEBUG, on lui dit qu'il doit utiliser le formateur
+# créé précédement et on ajoute ce handler au logger
+file_handler.setLevel(logging.DEBUG)
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
+
+# création d'un second handler qui va rediriger chaque écriture de log
+# sur la console
+# stream_handler = logging.StreamHandler()
+# stream_handler.setLevel(logging.DEBUG)
+# logger.addHandler(stream_handler)
---
+++ helpers/tcp_socket_server.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+
+
+import socketserver
+import threading
+import os
+import sys
+from setproctitle import setproctitle, getproctitle
+
+from dotenv import load_dotenv
+from .cdr import parse_cdr
+from .logging import logger
+
+
+class traitementDonnées(socketserver.BaseRequestHandler):
+    def __init__(self, request, client_address, server):
+        socketserver.BaseRequestHandler.__init__(self, request,
+                                                 client_address, server)
+        logger.info('handler init')
+        return
+
+    def handle(self):
+        cdr = self.request.recv(2048)
+        cdr = cdr.decode().strip()
+        self.request.send(bytes(cdr, 'utf-8'))
+        logger.info(cdr)
+        logger.info(parse_cdr(cdr))
+        if cdr == 'shutdown':
+            self.request.close()
+            threading.Thread(target=self.server.shutdown).start()
+        self.request.close()
+        return
+
+
+class serveur(socketserver.ThreadingMixIn, socketserver.TCPServer):
+    # Ctrl-C will cleanly kill all spawned threads
+    daemon_threads = True
+    # much faster rebinding
+    allow_reuse_address = True
+
+    def __init__(self, server_address, RequestHandlerClass):
+        socketserver.TCPServer.__init__(self,
+                                        server_address,
+                                        RequestHandlerClass)
+
+    def runserver():
+        load_dotenv()
+        HOST = '0.0.0.0'
+        PORT = int(os.environ.get('SERVER_PORT'))
+        tcpsrv = serveur((HOST, PORT), traitementDonnées)
+        setproctitle('3cxtcpserver')
+        log = 'Server loop ' + getproctitle() \
+            + ' running in process: ' + str(os.getpid())
+        logger.info(log)
+        try:
+            tcpsrv.serve_forever()
+        except KeyboardInterrupt:
+            sys.exit(0)
