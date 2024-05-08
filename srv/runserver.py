d20423da58b3551f44fda5085f7100e1b6ca8c91
David Orel <david.orel@sfr.fr>
2024-05-08 19:03:43 +0200

    Ajout SFTP et SCP #18

--- srv/runserver.py
+++ srv/runserver.py
@@ -26,3 +26,29 @@ if __name__ == '__main__':
                      filefolder,
                      archivefolder,
                      int(os.environ.get('FTP_3CX_INTERVAL')))
+    elif server_type=='SFTP':
+        print('SFTP client')
+        from myhelpers.sftpclient import sftpclient
+        sftpc = sftpclient(host=os.environ.get('FTP_3CX_HOST'),
+                         user=os.environ.get('FTP_3CX_LOGIN'),
+                         password=os.environ.get('FTP_3CX_PASSWORD'),
+                         port=os.environ.get('FTP_3CX_PORT'),
+                         private_key=os.environ.get('FTP_3CX_PRIVATE_KEY_PATH'),
+                         private_key_pass=os.environ.get('FTP_3CX_PRIVATE_KEY_PASSWORD'))
+        print('Ftp conected') 
+        sftpc.monitor(os.environ.get('FTP_3CX_SRVDIR'),
+                     filefolder,
+                     archivefolder,
+                     int(os.environ.get('FTP_3CX_INTERVAL')))
+    elif server_type=='SCP':
+        print('SCP client')
+        from myhelpers.scpclient import scpclient
+        scpc = scpclient(host=os.environ.get('SCP_3CX_HOST'),
+                         user=os.environ.get('SCP_3CX_LOGIN'),
+                         password=os.environ.get('SCP_3CX_PASSWORD'),
+                         port=os.environ.get('SCP_3CX_PORT'))
+        print('scp conected') 
+        scpc.monitor(os.environ.get('SCP_3CX_SRVDIR'),
+                     filefolder,
+                     archivefolder,
+                     int(os.environ.get('SCP_3CX_INTERVAL')))
