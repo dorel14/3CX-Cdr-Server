742bc9322507affece7e1051e4fc2322456064cd
David Orel <david.orel@sfr.fr>
2024-06-01 17:57:55 +0200

    Validation des permissions sur les volumes #27

--- myhelpers/traitement_fichier.py
+++ myhelpers/traitement_fichier.py
@@ -10,20 +10,28 @@ from myhelpers.logging import logger
 #filefolder = '/opt/cdrfiles'
 #savefolder = '/opt/cdrfiles_archives'
 
+def check_directory_permissions(directory_path):
+    permissions = os.stat(directory_path).st_mode
+    logger.error(f"Permissions of the directory:{directory_path}")
+    logger.error(f"Read permission: {'Yes' if permissions & 0o400 else 'No'}")
+    logger.error(f"Write permission: {'Yes' if permissions & 0o200 else 'No'}")
+    logger.error(f"Execute permission: {'Yes' if permissions & 0o100 else 'No'}")
+
 def files_move(file, savefolder):
     filename = str(os.path.basename(file))
     logger.info(filename)
     year = datetime.now().strftime("%Y")
     month = datetime.now().strftime("%m")
     date = datetime.now().strftime("%d-%m-%Y_%H-%M-%S")
+    check_directory_permissions(savefolder)
     if not os.path.exists(savefolder):
-        os.mkdir(savefolder)
-    savefolderd = savefolder + '/' + year + '/'
+        os.mkdir(savefolder, mode=0o777, exist_ok=False)
+    savefolderd = os.path.join(savefolder, year)
     if not os.path.exists(savefolderd):
-        os.mkdir(savefolderd)
-    savefolderd = savefolder + '/'+ year + '/' + month + '/'
+        os.mkdir(savefolderd, mode=0o777, exist_ok=False)
+    savefolderd = os.path.join(savefolderd, month)
     if not os.path.exists(savefolderd):
-        os.mkdir(savefolderd)
+        os.mkdir(savefolderd, mode=0o777, exist_ok=False)
     shutil.move(file, savefolderd + date + '_' + filename)  # to move files from
     logger.info(file + ' moved')
 
