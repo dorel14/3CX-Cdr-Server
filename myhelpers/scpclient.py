c1a3afb50c7e529b3cbd8e5fda451aafcb59bd9e
David Orel <david.orel@sfr.fr>
2024-06-09 20:17:07 +0200

    Refactoring de code + documentation de code + test correction de bug #31

--- myhelpers/scpclient.py
+++ myhelpers/scpclient.py
@@ -8,13 +8,32 @@ from myhelpers.logging import logger
 from myhelpers.traitement_fichier import csv_files_read
 
 class scpclient():
+    """
+    Provides a class `scpclient` that handles connecting to an FTP server, downloading files that are newer than the local versions, and optionally archiving or deleting the downloaded files on the FTP server.
+    """
     def __init__(self, host, user, password, port=22):
+        """
+        Initializes an instance of the `scpclient` class with the specified FTP server host, username, and password.
+        """
         self.host=host
         self.user=user
         self.password=password
         self.port=port
     
     def monitor(self, ftpfolder='', localfolder='',archivefolder='', interval=50):
+        """
+        Monitors an FTP folder, downloads any new files matching a specified file extension, and optionally archives or deletes the downloaded files on the FTP server.
+        
+        Args:
+            ftpfolder (str): The path to the FTP folder to monitor.
+            localfolder (str): The local folder to download files to.
+            archivefolder (str): The local folder to archive downloaded files to.
+            interval (int): The number of seconds to wait between checks for new files.
+        
+        Returns:
+            None
+        """
+                
         ssh = paramiko.SSHClient()
         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         ssh.connect(hostname=self.host, port=self.port, username= self.user, password= self.password, banner_timeout=200)
--- myhelpers/sftpclient.py
+++ myhelpers/sftpclient.py
@@ -5,7 +5,21 @@ from time import sleep
 from myhelpers.logging import logger
 from myhelpers.traitement_fichier import csv_files_read
 
+from pathlib import Path
+path = Path(__file__).resolve()
+dir_path = path.parent
+working_path = path.cwd()
 class sftpclient():
+    """
+    The `sftpclient` class provides a convenient way to interact with an SFTP server. It allows you to:
+    
+    - Connect to an SFTP server with the provided host, user, password, and optional port, private key, and private key password.
+    - Monitor an SFTP folder, download any new files to a local folder, and optionally archive or delete the files on the SFTP server.
+    - Read any CSV files that were downloaded to the local folder and move them to an archive folder.
+    
+    The `monitor` method is the main entry point for interacting with the SFTP server. It will connect to the SFTP server, change to the specified `ftpfolder`, download any new files to the `localfolder`, and then either archive or delete the files on the SFTP server based on the value of the `FTP_3CX_ARCHIVE_OR_DELETE` environment variable. Finally, it will read any CSV files that were downloaded and move them to the `archivefolder`.
+    """
+        
     def __init__(self, host, user, password, port=22, private_key=None, private_key_pass=None):
         self.host=host
         self.user=user
@@ -15,6 +29,19 @@ class sftpclient():
         self.private_key_pass = private_key_pass
     
     def monitor(self, ftpfolder='', localfolder='',archivefolder='', interval=50):
+        """
+        Monitors an SFTP folder, downloads any new files to a local folder, and optionally archives or deletes the files on the SFTP server. It then reads any CSV files that were downloaded and moves them to an archive folder.
+        
+        Args:
+            ftpfolder (str): The SFTP folder to monitor.
+            localfolder (str): The local folder to download files to.
+            archivefolder (str): The folder to move CSV files to.
+            interval (int): The number of seconds to wait between checks.
+        
+        Returns:
+            None
+        """
+                
         with pysftp.Connection(hostname=self.host, port=self.port,
                                username=self.user, password=self.password, 
                                private_key=None, private_key_pass=None) as sftp :
